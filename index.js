92fd649f02733eb83ef9271c98cdb3c759ac5e40
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-29 15:45:03 +0300

    adds loops

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+let writeCards = (list, events) => {
+  let myArr = [];
+  for (let i = 0; i < list.length; i++) {
+    myArr.push(`Thank you, ${list[i]}, for the wonderful ${events} gift!`);
+  }
+  return myArr;
+};
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) {
+    console.log(i);
+  }
+}
